a52657f4ea06525e8e118e66837db46a1b898944
Amir mashmool <48226769+amirmashmool@users.noreply.github.com>
2023-02-25 17:36:49 +0100

    Add files via upload

---
+++ static/js/top_buttons.js
@@ -0,0 +1,29 @@
+const pages = [
+    ["Home", "index.html"],
+    ["First Assignment", "Assignment1Page.html"],
+    ["Second Assignment", "Assignment2Page.html"],
+    ["Third Assignment", "Assignment3Page.html"],
+    ["Fourth Assignment", "Assignment4Page.html"]
+]
+
+d3.select("#navigation")
+    .style("position", "fixed")
+    .style("x", 0)
+    .style("width", "100%")
+    .style("display", "flex")
+    .style("flex-wrap", "wrap")
+    .style("flex-direction", "row")
+    .style("justify-content", "center")
+    .selectAll("buttons")
+    .data(pages)
+    .enter()
+    .append("a")
+    .attr("href", (d) => d[1])
+    .append("input")
+    .attr("type", "button")
+    .attr("value", (d) => d[0])
+    .attr("class", "button1")
+    .style("padding-right", "12px")
+    .style("padding-left", "12px")
+    .style("border", "solid")
+    .style("border-top", "none")
